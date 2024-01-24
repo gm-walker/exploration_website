97f8c884ce16198e1b9872159cde1c095c9e547d
GEE <gm.walker2022@gmail.com>
2024-01-24 02:26:31 -0500

    <feat> Updated styling & links of Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function Navbar(){
     return(
-        <nav>
-            <a href=""></a><a href=""></a>
+        <nav className="flex-auto gap-9 bg-zinc-700/50 p-5 m-5 rounded-xl *:rounded-xl *:p-3 *:mr-5 *:text-zinc-400  justify-around">
+            <Link href="https://discord.com/" className="hover:text-zinc-200 hover:ring-zinc-500 hover:ring-2">Discord</Link>
+            <Link href="https://github.com/gm-walker" className="hover:text-zinc-200 hover:ring-zinc-500/50 hover:ring-2">GitHub</Link>
+            <Link href=""></Link>
         </nav>
     );
 }
