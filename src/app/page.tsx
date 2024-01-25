2ac4a6db7543b54c0c91aa3f18068bf5a8313ae2
GEE <gm.walker2022@gmail.com>
2024-01-25 13:41:36 -0500

    <sty,feat> Added Hero Component to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Navbar from "@/components/navbar";
+import Hero from "@/components/hero";
 import Image from "next/image";
-Navbar
+
 
 export default function Home() {
   return (
     <main className="flex m-8">
       <div className="">
         <h1 className="text-3xl font-extrabold text-center tracking-widest leading-relaxed">Welcome to My Website</h1>
+        <Hero />
         <p className="leading-tight text-sm font-mono text-zinc-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque architecto, quo alias eveniet aperiam culpa ullam at praesentium repudiandae odio fuga dolor molestiae suscipit doloribus. Doloremque suscipit delectus nostrum soluta?</p>
       </div>
       <div className=""></div>
