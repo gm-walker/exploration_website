b15f35eb665f0fc53e188411b778336ed3118239
GEE <gm.walker2022@gmail.com>
2024-01-29 12:03:44 -0500

    <feat> Split up the About text from the Hero and Headline - Created IDs to define each section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex m-8">
+    <main id="intro"className="flex m-8">
       <div className="">
         <h1 className="text-3xl font-extrabold text-center tracking-widest leading-relaxed">Welcome to My Website</h1>
         <Hero />
-        <p className="leading-tight text-sm font-mono text-zinc-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque architecto, quo alias eveniet aperiam culpa ullam at praesentium repudiandae odio fuga dolor molestiae suscipit doloribus. Doloremque suscipit delectus nostrum soluta?</p>
       </div>
-      <div className=""></div>
+      <div id="about" className="">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestiae quos vel beatae eum saepe laudantium enim ad temporibus, tempore eveniet optio perspiciatis. Odio suscipit dignissimos nesciunt vero provident alias.</p>
+      </div>
     </main>
   );
 }
