9ee357b5fd968125f8c95e54a89b1053d41c9727
GEE <gm.walker2022@gmail.com>
2024-01-30 19:13:43 -0500

    <feat> Updating Hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 
 export default function Hero(){
     return(
-        <div>
-            <p>This is where the hero would be...</p>
+        <div className="">
+             <Image src={"/public/faded_gallery-RDnPOL6LViY-unsplash.jpg"} alt="" height={1080} width={1920}/>
         </div>
     );
 }
