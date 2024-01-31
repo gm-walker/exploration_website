fbc3c2816901da77acfe050d317e58621fb37f42
GEE <gm.walker2022@gmail.com>
2024-01-30 19:12:25 -0500

    <feat> Updating Homepage, Removing About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main id="intro"className="flex m-8">
+    <main id="intro"className="flex m-8 max-w-full justify-center text-center">
       <div className="">
         <h1 className="text-3xl font-extrabold text-center tracking-widest leading-relaxed">Welcome to My Website</h1>
         <Hero />
-      </div>
-      <div id="about" className="">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestiae quos vel beatae eum saepe laudantium enim ad temporibus, tempore eveniet optio perspiciatis. Odio suscipit dignissimos nesciunt vero provident alias.</p>
       </div>
     </main>
   );
