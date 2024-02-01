8d08600943624c78dd1637084cb29a83fb82f832
GEE <gm.walker2022@gmail.com>
2024-02-01 00:00:11 -0500

    <feat> Removed Hero Image

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Hero(){
     return(
         <div className="">
-             <Image src={"/public/faded_gallery-RDnPOL6LViY-unsplash.jpg"} alt="" height={1080} width={1920}/>
         </div>
     );
 }
