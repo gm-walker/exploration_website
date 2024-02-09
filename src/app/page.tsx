7fe0325c3101784200d8cc7efd53a0aca39c6bcf
GEE <gm.walker2022@gmail.com>
2024-02-09 17:32:59 -0500

    <feat> Adding an Image Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Hero from "@/components/hero";
 import Image from "next/image";
 
 
 export default function Home() {
   return (
     <main id="intro"className="flex m-8 max-w-full justify-center text-center">
-      <div className="">
+      <div className="overflow-hidden">
         <h1 className="text-3xl font-extrabold text-center tracking-widest leading-relaxed">Welcome to My Website</h1>
-        <Hero />
+        
+        <div className="h-2/3">
+        <img alt="Hero Image" src="https://images.unsplash.com/photo-1691084041747-9cc0ade8ceda?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="100%"/>
+        </div>
       </div>
     </main>
   );
